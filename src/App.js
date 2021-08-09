654d5a2cb9b0f86ce44976cc4385cd8b8c5808ca
gbezzuoli <guilhermebezzuoli@outlook.com>
2021-08-09 15:16:17 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1></h1>
+        <h1>a</h1>
       </div>
-    )
+    );
   }
 }
