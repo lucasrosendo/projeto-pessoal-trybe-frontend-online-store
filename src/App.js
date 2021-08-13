c2cca7e61ce2380f4da22b7a9ba7e942b5b6e22a
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-08-13 17:30:44 -0300

    10. Visualize a lista de produtos adicionados ao carrinho em sua página e permita a manipulação da sua quantidade

--- src/App.js
+++ src/App.js
@@ -15,11 +15,37 @@ export default class App extends Component {
   }
 
   addToCart = (product) => {
+    const newProduct = {
+      id: product.id,
+      thumbnail: product.thumbnail,
+      title: product.title,
+      price: product.price,
+      quantity: 1,
+    };
     this.setState((prevState) => ({
-      cart: [...prevState.cart, product],
+      cart: [...prevState.cart, newProduct],
       empty: false,
     }));
   };
+
+  handleQuantity = (event, id) => {
+    const { cart } = this.state;
+    const { value } = event.target;
+    const ProductQuantity = cart.map((newProductQtt) => {
+      if (newProductQtt.id === id && value === '+') {
+        newProductQtt.quantity += 1;
+        return newProductQtt;
+      }
+      if (newProductQtt.id === id && value === '-' && newProductQtt.quantity > 1) {
+        newProductQtt.quantity -= 1;
+        return newProductQtt;
+      }
+      return newProductQtt;
+    });
+    this.setState({
+      cart: ProductQuantity,
+    });
+  }
 
   cartItemRemove = (id) => {
     const { cart } = this.state;
@@ -43,6 +69,7 @@ export default class App extends Component {
               <ShoppingCart
                 cart={ cart }
                 empty={ empty }
+                handleQuantity={ this.handleQuantity }
                 cartItemRemove={ this.cartItemRemove }
               />) }
           />
