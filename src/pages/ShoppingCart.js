7399d34f94a8b3add3f7f6c3b248ea6da1472ba2
Japa <wesleyy_@live.com>
2021-08-13 18:02:19 -0300

    att req-10

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+
+    const { cart } = this.props;
+    this.state = {
+      cart,
+    };
+
+  }
+
   render() {
     const { cart, empty, cartItemRemove } = this.props;
 
@@ -13,7 +23,21 @@ class ShoppingCart extends Component {
               <img src={ thumbnail } alt="imagem" />
               <div>
                 <h4 data-testid="shopping-cart-product-name">{title}</h4>
-                <h5 data-testid="shopping-cart-product-quantity">1</h5>
+                <div>
+                  <input
+                    data-testid="product-increase-quantity"
+                    type="button"
+                    onClick={ }
+                    value="+"
+                  />
+                  <h5 data-testid="shopping-cart-product-quantity">{}</h5>
+                  <input
+                    data-testid="product-decrease-quantity"
+                    type="button"
+                    onClick={ }
+                    value="-"
+                  />
+                </div>
                 <h5>{price}</h5>
               </div>
             </div>
